e00514ac8bfbcc83899861506fefc3b26f2a256e
nasudeng <57230666+nasudeng@users.noreply.github.com>
2020-05-18 00:09:00 +0900

    6月用の乱数作成スクリプト

--- make_1hdraw_list.py
+++ make_1hdraw_list.py
@@ -2,40 +2,60 @@ import random
 import pickle
 import statistics
 import itertools
+import collections
 
 def makelist():
-  lis = [[random.randint(0,len(talents)-1) for i in range(3)] for i in range(100)]
 
-  lis_notdup = []
+  while(True):
+    lis = [[random.randint(0,len(talents)-1) for i in range(3)] for i in range(3)]
+    chk_lis =  list(itertools.chain.from_iterable(lis))
+    c = collections.Counter(chk_lis)
+    if (max(c.values())) == 1:
+      break
 
-  for ii in lis:
-    if ii[0]!=ii[1] and ii[1]!=ii[2] and ii[2]!=ii[0]:
-      lis_notdup.append(sorted(ii))
-  lis_fin = [lis_notdup[0]]
+  lis_3days = lis.copy()
 
-  for ii in lis_notdup:
-    flag = 0
-    for tt in range(3):
-      flag += int(ii[tt] in lis_fin[-1])
-
-    if flag<1:
-      lis_fin.append(ii)
-
-  return lis_fin[0:31]
-
-if __name__=="__main__":
+  while(True):
 
 
     while(True):
-      liss = makelist()
-      std = statistics.stdev(itertools.chain.from_iterable(liss))
-      d = 0.00001
-      if (std < (6.493586579592718*(1+d))) and (std > (6.493586579592718*(1-d))):
+      lis_new = [random.randint(0,len(talents)-1) for i in range(3)]
+      if lis_new[0]!=lis_new[1] and lis_new[1]!=lis_new[2] and lis_new[2]!=lis_new[0]:
         break
 
-    with open("May2020",mode="wb") as file:
+    lis_flat = list(itertools.chain.from_iterable(lis_3days))
+
+    if not((lis_new[0] in lis_flat) or (lis_new[1] in lis_flat) or (lis_new[2] in lis_flat)):
+      lis.append(lis_new)
+
+      lis_3days.pop(0)
+      lis_3days.append(lis_new)
+
+    if len(lis) >= 31:
+      break
+
+  return lis
+
+
+
+if __name__=="__main__":
+
+    talents = []
+
+    talents = ["日ノ隈らん","因幡はねる","宗谷いちか","風見くく","柚原いづみ","灰原あかね","白宮みみ","羽柴なつみ","黒猫ななし","堰代ミコ","周防パトラ","島村シャルロット","西園寺メアリ","灰猫ななし","獅子王クリス","龍ヶ崎リン","虎城アンナ","三毛猫ななし","杏戸ゆげ","鴨見カモミ","季咲あんこ","花奏かのん","銀猫ななし"]
+
+    while(True):
+      liss = makelist()
+      chk_lis =  list(itertools.chain.from_iterable(liss))
+      c = collections.Counter(chk_lis)
+
+      if max(c.values())<=5 and min(c.values())>=3:
+        break
+
+
+    with open("June2020",mode="wb") as file:
         pickle.dump(liss,file)
-    with open("May2020.txt",mode="w") as file:
+    with open("June2020.txt",mode="w") as file:
         file.write(str(liss))
 
     print("\007")
